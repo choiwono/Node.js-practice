3a5b403ce4c7cbf82fb82d7b748ae5e176ce840e
choiwono <treasure_b@naver.com>
2019-06-30 16:41:13 +0900

    # node.js 교과서 공부 7 챕터까지

---
+++ learn-express2/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/:id',(req,res,next) => {
+  console.log(req.params, req.query);
+});
+
+module.exports = router;
